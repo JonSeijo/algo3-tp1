ba03e701b7626edef90c666bd7c08f37c3178c5e
jonseijo <jon.seijo@gmail.com>
2017-04-01 18:58:16 -0300

    Renamings

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # algo3-tp1
 Tp1 de algo3
+
+# IMPORTANTE:
+
+La compilacion del informe se hace de foma automatica con un script que convierte del formato de jupyter
+Esto es asi para poder insertar paquetes de latex que no se encuentran en el engine de jupyter
+(En particular algorithms para el pseudocodigo)
+
+La caratula entonces es inexistente por ser una compilacion automatica.
+En la entrega final, puede hacerse alindarse editando a mano el archivo Informe.tex dentro de jupyter/
+
+Comandos para compilar y ver el informe:
+
+```
+python makepdf.py
+
+evince Informe.pdf
+
+```
--- makepdf.py
+++ makepdf.py
@@ -5,8 +5,8 @@ import subprocess
 # Solution was just write latex imagining I had the packages, 
 # and then compile the pdf manually using this template to insert the packages declaration
 
-template_file = "jupyter/use_algorithms_latex_packs.tplx"
-notebook_name = "test"
+template_file = "jupyter/template_jupyterpdf.tplx"
+notebook_name = "Informe"
 notebook_path = "jupyter/" + notebook_name
 
 convert_jupyter_command = "jupyter nbconvert " + notebook_path  + ".ipynb" +  " --to latex " + "--template " + template_file
@@ -15,6 +15,7 @@ make_pdf_command = "pdflatex " + "-output-directory jupyter " + notebook_path + 
 move_pdf_command = "mv " + notebook_path + ".pdf " + notebook_name + ".pdf"
 
 subprocess.call(convert_jupyter_command.split())
+
+# Para la entrega final, modificar a mano el .tex para hacer una bonita caratula
 subprocess.call(make_pdf_command.split())
 subprocess.call(move_pdf_command.split())
-
