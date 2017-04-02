c17bf0f1de1a5d240a3b592452ec686602af2dfc
jonseijo <jon.seijo@gmail.com>
2017-04-02 15:10:12 -0300

    Alindar tiempos.cpp

--- backtracking_naive.cpp
+++ backtracking_naive.cpp
@@ -5,24 +5,7 @@
 
 #define INFINITO INT_MAX;
 
-std::vector<int> numeros;
-
-enum Color { 
-    Azul, Rojo, Ninguno
-};
-
-// Defino << para mostrar el color
-std::ostream& operator<<(std::ostream& os, Color c) {
-    switch(c) {
-        case Azul: os << "Azul"; break;
-        case Rojo: os << "Rojo"; break;
-        default: os << "Ninguno";
-    }
-    return os;
-}
-
-
-bool valido(std::vector<Color> &colores) {
+bool valido(const std::vector<int> &numeros, std::vector<Color> &colores) {
     int n = (int)numeros.size();
     int ultimoRojo = -1;
     int ultimoAzul = -1;
@@ -49,37 +32,37 @@ bool valido(std::vector<Color> &colores) {
 }
 
 // Devuelve la minima cantidad sin pintar posible
-int backtrack_naive(int actual, std::vector<Color> &colores,
+int backtrack_naive(const std::vector<int> &numeros, int actual, std::vector<Color> &colores,
              int ultimoRojo, int ultimoAzul, int cantSinPintar) {
     
     // Caso base, ya no hay mas numeros para pintar
     if (actual == (int)numeros.size()) {
-        return valido(colores) ? cantSinPintar : INFINITO;
+        return valido(numeros, colores) ? cantSinPintar : INFINITO;
     }
     
     int minimo = INFINITO;
     
     // Pinto de rojo
     colores[actual] = Rojo;
-    int minConRojo = backtrack_naive(actual+1, colores, actual, ultimoAzul, cantSinPintar);
+    int minConRojo = backtrack_naive(numeros, actual+1, colores, actual, ultimoAzul, cantSinPintar);
     minimo = std::min(minimo, minConRojo);
     
     // Pinto de azul 
     colores[actual] = Azul;
-    int minConAzul = backtrack_naive(actual+1, colores, ultimoRojo, actual, cantSinPintar);
+    int minConAzul = backtrack_naive(numeros, actual+1, colores, ultimoRojo, actual, cantSinPintar);
     minimo = std::min(minimo, minConAzul);
     
     // No pinto 
     colores[actual] = Ninguno;
-    int minSinPintar = backtrack_naive(actual+1, colores, ultimoRojo, ultimoAzul, cantSinPintar+1);
+    int minSinPintar = backtrack_naive(numeros, actual+1, colores, ultimoRojo, ultimoAzul, cantSinPintar+1);
     minimo = std::min(minimo, minSinPintar);
     
     return minimo;
 }
 
-int resolver_backtracking_naive(int n, std::vector<int> &numeros) {
+int resolver_backtracking_naive(int n, const std::vector<int> &numeros) {
     std::vector<Color> colores(n, Ninguno);
-    return backtrack_naive(0, colores, -1, -1, 0);
+    return backtrack_naive(numeros, 0, colores, -1, -1, 0);
 }
 
 // int main(int argc, char *argv[]) {
--- tiempos.cpp
+++ tiempos.cpp
@@ -1,4 +1,6 @@
+#include "definiciones.cpp"
 #include "backtracking_naive.cpp"
+#include "backtracking_poda.cpp"
 #include <chrono>
 
 // NOTA: Si quiero muchas ejecuciones para la misma entrada, 
@@ -10,11 +12,11 @@
  * 2) numeros del input separados por espacios
  */
 
+
 int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
     
-    // Numeros es global pues no cambia para ningun problema
-    numeros.resize(n, 0);
+    std::vector<int> numeros(n, 0);
 
     for (int i = 0; i < n; i++) {
         std::string dato = argv[i+2];
@@ -22,6 +24,7 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << resolver_backtracking_naive(n, numeros) << "\n";
+    std::cout << resolver_backtracking_poda(n, numeros) << "\n";
     
     return 0;
 }
