34ee7e1514d8dc0ef9142fc8b7ab81f4f4b76b0b
jonseijo <jon.seijo@gmail.com>
2017-04-02 15:31:07 -0300

    Actualizo script para crear informe para el nuevo workflow

--- makepdf.py
+++ makepdf.py
@@ -1,21 +1,12 @@
 import subprocess
 
-# Template file to include algorithms latex packages
-# The problem was that you cannot include external packages in jupyter engine
-# Solution was just write latex imagining I had the packages, 
-# and then compile the pdf manually using this template to insert the packages declaration
+informe_name = "informe"
+informe_directorio = "informe/"
 
-template_file = "jupyter/template_jupyterpdf.tplx"
-notebook_name = "Informe"
-notebook_path = "jupyter/" + notebook_name
+make_pdf_command = "pdflatex " + "-output-directory informe " + informe_name + ".tex"
+# move_pdf_command = "mv " + informe_directorio + informe_name + ".pdf " + informe_name + ".pdf"
+open_pdf_command = "evince " + informe_directorio + informe_name + ".pdf"
 
-convert_jupyter_command = "jupyter nbconvert " + notebook_path  + ".ipynb" +  " --to latex " + "--template " + template_file
-make_pdf_command = "pdflatex " + "-output-directory jupyter " + notebook_path + ".tex"
-
-move_pdf_command = "mv " + notebook_path + ".pdf " + notebook_name + ".pdf"
-
-subprocess.call(convert_jupyter_command.split())
-
-# Para la entrega final, modificar a mano el .tex para hacer una bonita caratula
 subprocess.call(make_pdf_command.split())
-subprocess.call(move_pdf_command.split())
+# subprocess.call(move_pdf_command.split())
+subprocess.call(open_pdf_command.split())
