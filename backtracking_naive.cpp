4a7d2e7259cf2f3f7f5f1d66cd06d8f54a122642
Jonathan Seijo <jonathan.seijo@avature.net>
2017-04-03 10:19:36 -0300

    Elimina contenido extra

--- backtracking_naive.cpp
+++ backtracking_naive.cpp
@@ -31,29 +31,29 @@ bool valido(const std::vector<int> &numeros, std::vector<Color> &colores) {
 // Devuelve la minima cantidad sin pintar posible
 int backtrack_naive(const std::vector<int> &numeros, int actual, std::vector<Color> &colores,
              int ultimoRojo, int ultimoAzul, int cantSinPintar) {
-    
+
     // Caso base, ya no hay mas numeros para pintar
     if (actual == (int)numeros.size()) {
         return valido(numeros, colores) ? cantSinPintar : INFINITO;
     }
-    
+
     int minimo = INFINITO;
-    
+
     // Pinto de rojo
     colores[actual] = Rojo;
     int minConRojo = backtrack_naive(numeros, actual+1, colores, actual, ultimoAzul, cantSinPintar);
     minimo = std::min(minimo, minConRojo);
-    
-    // Pinto de azul 
+
+    // Pinto de azul
     colores[actual] = Azul;
     int minConAzul = backtrack_naive(numeros, actual+1, colores, ultimoRojo, actual, cantSinPintar);
     minimo = std::min(minimo, minConAzul);
-    
-    // No pinto 
+
+    // No pinto
     colores[actual] = Ninguno;
     int minSinPintar = backtrack_naive(numeros, actual+1, colores, ultimoRojo, ultimoAzul, cantSinPintar+1);
     minimo = std::min(minimo, minSinPintar);
-    
+
     return minimo;
 }
 
@@ -61,15 +61,3 @@ int resolver_backtracking_naive(int n, const std::vector<int> &numeros) {
     std::vector<Color> colores(n, Ninguno);
     return backtrack_naive(numeros, 0, colores, -1, -1, 0);
 }
-
-// int main(int argc, char *argv[]) {
-//     int n = atoi(argv[1]);
-//     numeros.resize(n, 0);
-//     for (int i = 0; i < n; i++) {
-//         std::string dato = argv[i+2];
-//         numeros[i] = std::stoi(dato);
-//     }
-//     std::cout << resolver_backtracking_naive(n, numeros) << "\n";
-    
-//     return 0;
-// }
