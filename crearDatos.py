c0ebf05489cf9f63da62dca721a1bee97550aea0
jonseijo <jon.seijo@gmail.com>
2017-04-04 23:59:32 -0300

    Agrega soporte para algoritmo con poda

--- crearDatos.py
+++ crearDatos.py
@@ -28,7 +28,7 @@ def args_lista_random(tipo, tam, tope):
     return [ejecutable, tipo, str(tam)] + [str(random.randrange(1, tope+1)) for _ in range(tam)]
 
 def exp_random(tipo, archivo, tope):
-    with open(path_naive + archivo, 'w') as f:
+    with open(archivo, 'w') as f:
         for tam in range(1, n_maximo+1):   # Instancias de tamaño n
             print("Instancias random de tamaño: " + str(tam))
             print("\nn:" + str(tam), file=f)
@@ -42,7 +42,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-naive_todos", help="Todos los experimentos de naive", action='store_true')
     parser.add_argument("-naive_random_cercanos", help="Naive con listas random en rango 1-20", action='store_true')
-    parser.add_argument("-naive_random_lejanos", help="Naive con listas random en rango 1-100000", action='store_true')
+    parser.add_argument("-naive_random_lejanos", help="Naive con listas random en rango 1-200000", action='store_true')
+    parser.add_argument("-poda_random_cercanos", help="Poda con listas random en rango 1-20", action='store_true')
+    parser.add_argument("-poda_random_lejanos", help="Poda con listas random en rango 1-200000", action='store_true')
     parser.add_argument("--repes", help="Cantidad de repeticiones", default=repes, type=int)
 
     args = parser.parse_args()
@@ -51,13 +53,25 @@ if __name__ == '__main__':
     if args.naive_random_cercanos:
         print("Inicia: exp_naive_random_cercanos")
         start_time = time.time()
-        exp_random(tipo_naive, path_random_cercanos, 20)
+        exp_random(tipo_naive, path_naive +  path_random_cercanos, 20)
         print("--- %s seconds ---" % (time.time() - start_time))
 
     if args.naive_random_lejanos:   
         print("Inicia: exp_naive_random_lejanos")
         start_time = time.time()
-        exp_random(tipo_naive, path_random_lejanos, 200000)
+        exp_random(tipo_naive, path_naive + path_random_lejanos, 200000)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    if args.poda_random_cercanos:
+        print("Inicia: exp_naive_random_cercanos")
+        start_time = time.time()
+        exp_random(tipo_poda, path_poda + path_random_cercanos, 20)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    if args.poda_random_lejanos:   
+        print("Inicia: exp_naive_random_lejanos")
+        start_time = time.time()
+        exp_random(tipo_poda, path_poda + path_random_lejanos, 200000)
         print("--- %s seconds ---" % (time.time() - start_time))
 
 
