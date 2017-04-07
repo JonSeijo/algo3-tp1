6db92d9c0622a81fc22a9d926d25446abb7b70ae
Jonathan Seijo <jonathan.seijo@avature.net>
2017-04-07 10:20:09 -0300

    Agrego soporte para solucion dp

---
+++ dp.cpp
@@ -0,0 +1,15 @@
+// POR AHORA ES DP A SECAS PORQUE NO SE DE QUE FORMA LO VOY A IMPLEMENTAR
+
+#include <climits>
+
+#define INFINITO INT_MAX;
+
+
+int f() {
+    return 0;
+}
+
+
+int resolver_dp(int n, const std::vector<int> &numeros) {
+    return f();
+}
--- solucion.cpp
+++ solucion.cpp
@@ -1,10 +1,11 @@
 #include "definiciones.cpp"
 #include "backtracking_naive.cpp"
 #include "backtracking_poda.cpp"
+#include "dp.cpp"
 
 int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
-    
+
     std::vector<int> numeros(n, 0);
 
     for (int i = 0; i < n; i++) {
@@ -14,7 +15,8 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Solucion bt naive: " << resolver_backtracking_naive(n, numeros) << "\n";
     std::cout << "Solucion bt poda: " << resolver_backtracking_poda(n, numeros) << "\n";
-    
+    std::cout << "Solucion DP: " << resolver_dp(n, numeros) << "\n";
+
     return 0;
 }
 
