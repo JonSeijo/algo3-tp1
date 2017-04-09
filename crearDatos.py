9742b33e3563a04e09d4f35df90db28ca51ba7a1
jonseijo <jon.seijo@gmail.com>
2017-04-09 13:11:56 -0300

    Agrego soporte para obtener datos de DP

--- crearDatos.py
+++ crearDatos.py
@@ -12,6 +12,8 @@ ejecutable = "./tiempos"
 
 path_naive = "datos/naive/"
 path_poda = "datos/poda/"
+path_topdown = "datos/topdown/"
+path_bottomup = "datos/bottomup/"
 
 path_random = "random.csv"
 
@@ -21,9 +23,11 @@ tipo_topdown = "topdown"
 tipo_bottomup = "bottomup"
 
 repes = 25
-cota_valor_maximo = 200000
-n_maximo_naive = 15
-n_maximo_poda = 32
+cota_valor_maximo = 1000000
+n_maximo_naive = 15 
+n_maximo_poda = 32 # Con 32 tarda 5 segundos
+n_maximo_topdown = 90 # Con 100 tarda 1 minuto
+n_maximo_bottomup = 90
 
 def args_lista_random(tipo, n):
     return [ejecutable, tipo, str(n)] + [str(random.randrange(1, cota_valor_maximo+1)) for _ in range(n)]
@@ -41,9 +45,21 @@ def exp_random(tipo, archivo, tamMaximo):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    
     parser.add_argument("-naive_todos", help="Todos los experimentos de naive", action='store_true')
-    parser.add_argument("-naive_random", help="Naive con listas random. Elementos con cota " + str(cota_valor_maximo), action='store_true')
-    parser.add_argument("-poda_random", help="Poda con listas random. Elementos con cota " + str(cota_valor_maximo), action='store_true')
+    
+    parser.add_argument("-naive_random", help="Naive con listas random."
+                + " Tamaño maximo: " + str(n_maximo_naive), action='store_true')
+
+    parser.add_argument("-poda_random", help="Poda con listas random."
+                + " Tamaño maximo: " + str(n_maximo_poda), action='store_true')
+
+    parser.add_argument("-topdown_random", help="Topdown con listas random."
+             + " Tamaño maximo: " + str(n_maximo_topdown), action='store_true')
+
+    parser.add_argument("-bottomup_random", help="Bottomup con listas random."
+             + " Tamaño maximo: " + str(n_maximo_bottomup), action='store_true')
+
     parser.add_argument("--repes", help="Cantidad de repeticiones", default=repes, type=int)
 
     args = parser.parse_args()
@@ -59,4 +75,16 @@ if __name__ == '__main__':
         print("Inicia: exp_poda_random")
         start_time = time.time()
         exp_random(tipo_poda, path_poda + path_random, n_maximo_poda)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    if args.topdown_random:
+        print("Inicia: exp_topdown_random")
+        start_time = time.time()
+        exp_random(tipo_topdown, path_topdown + path_random, n_maximo_topdown)
+        print("--- %s seconds ---" % (time.time() - start_time))
+
+    if args.bottomup_random:
+        print("Inicia: exp_bottomup_random")
+        start_time = time.time()
+        exp_random(tipo_bottomup, path_bottomup + path_random, n_maximo_bottomup)
         print("--- %s seconds ---" % (time.time() - start_time))
