0d7b39332d5430fedf55d3fdf9a48517cc8529a5
jonseijo <jon.seijo@gmail.com>
2017-04-10 23:03:49 -0300

    Reemplaza -1 por n para consistencia

--- backtracking_poda.cpp
+++ backtracking_poda.cpp
@@ -3,8 +3,10 @@ int backtrack_poda_minimo_global = INFINITO;
 // Devuelve la minima cantidad sin pintar posible
 int backtrack_poda(const std::vector<int> &numeros, int actual, int ultimoRojo, int ultimoAzul, int cantSinPintar) {
 
+    int n = (int)numeros.size();
+
     // Caso base, ya no hay mas numeros para pintar
-    if (actual == (int)numeros.size()) {
+    if (actual == n) {
         backtrack_poda_minimo_global = cantSinPintar;
         return cantSinPintar;
     }
@@ -13,14 +15,14 @@ int backtrack_poda(const std::vector<int> &numeros, int actual, int ultimoRojo, 
 
     // Pinto de rojo solo si el numero es mayor al anterior rojo
     // (Los rojos deben ser estrictamente crecientes)
-    if (ultimoRojo == -1 || numeros[actual] > numeros[ultimoRojo]) {
+    if (ultimoRojo == n || numeros[actual] > numeros[ultimoRojo]) {
         int minConRojo = backtrack_poda(numeros, actual+1, actual, ultimoAzul, cantSinPintar);
         minimo = std::min(minimo, minConRojo);
     }
 
     // Pinto de azul solo si el numero es menor al anterior azul
     // (Los azules deben ser estrictamente decrecientes)
-    if (ultimoAzul == -1 || numeros[actual] < numeros[ultimoAzul]) {
+    if (ultimoAzul == n || numeros[actual] < numeros[ultimoAzul]) {
         int minConAzul = backtrack_poda(numeros, actual+1, ultimoRojo, actual, cantSinPintar);
         minimo = std::min(minimo, minConAzul);
     }
@@ -36,5 +38,5 @@ int backtrack_poda(const std::vector<int> &numeros, int actual, int ultimoRojo, 
 
 int resolver_backtracking_poda(int n, const std::vector<int> &numeros) {
     std::vector<Color> colores(n, Ninguno);
-    return backtrack_poda(numeros, 0, -1, -1, 0);
+    return backtrack_poda(numeros, 0, n, n, 0);
 }
